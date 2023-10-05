38660d478e796becf3feca132255d29eb46c054f
Puneet Arya <puneetarya90@gmail.com>
2023-10-05 10:31:38 +0530

    refactor added session functionality

--- django_demo_project/tasks/views.py
+++ django_demo_project/tasks/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-tasks = ['foo', 'bar', 'main']
+#tasks = ['foo', 'bar', 'main']
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
@@ -12,8 +12,10 @@ class NewTaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session['tasks'] = []
     return render(request, "tasks/index.html", {
-        "tasks":tasks
+        "tasks":request.session['tasks']
     } )
 
 def add(request):
