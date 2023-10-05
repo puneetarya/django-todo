02b7991db373eed2bab080a4556e8efc2be90e01
Puneet Arya <puneetarya90@gmail.com>
2023-10-05 10:42:49 +0530

    refactor session specific task display

--- django_demo_project/tasks/views.py
+++ django_demo_project/tasks/views.py
@@ -23,7 +23,7 @@ def add(request):
         form  = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data['task']
-            tasks.append(task)
+            request.session['tasks'] +=[task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
