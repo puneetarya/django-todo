0c801493fcc2dd38717547e38f157cdc76cd347c
Puneet Arya <puneetarya90@gmail.com>
2023-10-26 11:08:46 +0530

    added model airport and flight

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} {self.code}"
+
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, related_name="departures", on_delete=models.CASCADE)
+    destination = models.ForeignKey(Airport, related_name="arrivals", on_delete=models.CASCADE)
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
