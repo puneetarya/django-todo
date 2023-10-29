64451ee2d758352676340d190e1fbea927e58488
Puneet Arya <puneetarya90@gmail.com>
2023-10-29 20:01:36 +0530

    added url and views for linechart with base htmls

--- django_charts/linechart/urls.py
+++ django_charts/linechart/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("chart", views.chart, name="linechart")
+    path("plot", views.plot, name="linechart")
 ]
--- django_charts/linechart/views.py
+++ django_charts/linechart/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'linechart/index.html')
+
+def plot(request):
+    return render(request, 'linechart/linechart.html')
