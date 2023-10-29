e569846879539749d82c67335b3acc48eb05daa3
Puneet Arya <puneetarya90@gmail.com>
2023-10-29 22:53:26 +0530

    added a data model total candidates used schema from ae_contested_deposit_losts file

--- django_charts/barchart/models.py
+++ django_charts/barchart/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class TotalCandidates(models.Model):
+    State_Name = models.TextField(max_length=50, blank=True)
+    Assembly_No = models.IntegerField(blank=True)
+    Year = models.IntegerField(blank=True)
+    Total_Candidate = models.IntegerField(blank=True)
+    Deposit_Lost = models.IntegerField(blank=True)
+
+    class Meta:
+        verbose_name_plural = 'ae_contested_deposit_losts'
+
+    def __str__(self) -> str:
+        return f'{self.Year}--{self.Total_Candidate}'
