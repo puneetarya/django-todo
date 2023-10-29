34faf4c33b0510db4a26c8c0301aa017b995a83a
Puneet Arya <puneetarya90@gmail.com>
2023-10-29 22:56:31 +0530

    refactor to access model data to render

--- django_charts/barchart/views.py
+++ django_charts/barchart/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import TotalCandidates
 
 # Create your views here.
 def index(request):
     return render(request, 'barchart/index.html')
 
 def plot(request):
-    return render(request, 'barchart/barchart.html')
+
+    data = TotalCandidates.objects.all()
+    context = {
+        'data':data
+    }
+
+    return render(request, 'barchart/barchart.html', context)
