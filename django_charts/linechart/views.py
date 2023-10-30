7d9fe0b9c191320c442db9a91c5e64274ac25f79
Puneet Arya <puneetarya90@gmail.com>
2023-10-30 13:05:57 +0530

    refactor view for index to get uploaded file

--- django_charts/linechart/views.py
+++ django_charts/linechart/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-
+from django.core.files.storage import FileSystemStorage
+import os
 # Create your views here.
 
 def index(request):
+    context = {}
+    if request.method == "POST":
+        uploaded_file = request.FILES['document']
+
+        print(uploaded_file)
+
+        # check file type must be csv only
+        if uploaded_file.name.endswith('.csv'):
+            print("Upload CSV file only")
+            savefile = FileSystemStorage()
+            name = savefile.save(uploaded_file.name, uploaded_file)
+            # where to save files
+
+            #d = os.getcwd() # current directory of the project
+            #file_directory = os.path.join(d, 'media', name)
+            #print(file_directory)
+        # save file in media folder
     return render(request, 'linechart/index.html')
 
 def plot(request):
     return render(request, 'linechart/linechart.html')
+
