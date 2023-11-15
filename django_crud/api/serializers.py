9a4bbf29ccd0f24b823855255ae14d0bb997a2b1
Puneet Arya <puneetarya90@gmail.com>
2023-11-15 11:12:45 +0530

    Refactor added validators
    custom functions for validations. They could be used repeatedly with
    other fields or serializers
    call this functions for the desired field.

--- django_crud/api/serializers.py
+++ django_crud/api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Student
 
+def start_with_r(value):
+    if value[0].lower() != 'r':
+        raise serializers.ValidationError('Name should start with r')
+    
+
 class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=100, validators=[start_with_r])
     roll = serializers.IntegerField()
     city = serializers.CharField(max_length=100)
 
